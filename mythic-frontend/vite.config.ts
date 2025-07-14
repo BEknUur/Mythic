cf9f7b68095b7a9a8d9d4d715e74b82b35d61243
Beknur <ualihanulybeknur@gmail.com>
2025-07-14 18:55:01 +0500

    fix

--- mythic-frontend/vite.config.ts
+++ mythic-frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,6 +12,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
       '/view': {
         target: 'http://localhost:8000',
         changeOrigin: true,
@@ -21,7 +25,3 @@ export default defineConfig({
     },
   },
 })
-
-
-
-
