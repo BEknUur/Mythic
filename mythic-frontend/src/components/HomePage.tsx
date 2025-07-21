c0a8f10bdec25b72c494d990f557c0d300771a07
Beknur <ualihanulybeknur@gmail.com>
2025-07-21 11:34:57 +0500

    add the photo

--- mythic-frontend/src/components/HomePage.tsx
+++ mythic-frontend/src/components/HomePage.tsx
@@ -1,12 +1,10 @@
 import { Hero } from './Hero';
-import { Testimonials } from './Testimonials';
 import { Footer } from './Footer';
 
 export function HomePage() {
   return (
     <>
       <Hero />
-      <Testimonials />
       <Footer />
     </>
   );
